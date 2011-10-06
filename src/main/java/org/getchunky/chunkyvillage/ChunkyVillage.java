8e521fac23a4998f0834248048a9c10f5ad4a51b
SwearWord <theswearword@gmail.com>
2011-10-06 16:33:52 -0400

    Various edits to main class

--- src/main/java/org/getchunky/chunkyvillage/ChunkyVillage.java
+++ src/main/java/org/getchunky/chunkyvillage/ChunkyVillage.java
@@ -1,25 +1,38 @@
 package org.getchunky.chunkyvillage;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.getchunky.chunky.Chunky;
 import org.getchunky.chunky.event.ChunkyEvent;
 import org.getchunky.chunky.exceptions.ChunkyUnregisteredException;
 import org.getchunky.chunky.module.ChunkyCommand;
-import org.getchunky.chunkyvillage.commands.ForSale;
-import org.getchunky.chunkyvillage.commands.NewTown;
-import org.getchunky.chunkyvillage.commands.Town;
+import org.getchunky.chunkyvillage.commands.*;
+import org.getchunky.chunkyvillage.commands.set.Set;
+import org.getchunky.chunkyvillage.commands.set.SetName;
+import org.getchunky.chunkyvillage.commands.toggle.Toggle;
 import org.getchunky.chunkyvillage.listeners.ChunkyEvents;
 
 import java.util.Arrays;
 
 public class ChunkyVillage extends JavaPlugin {
     private ChunkyEvents chunkyEvents;
+
+    private static Plugin plugin;
+
+    public static Plugin getInstance() {
+        return plugin;
+    }
+
     public void onDisable() {
         // TODO: Place any custom disable code here.
         System.out.println(this + " is now disabled!");
     }
 
     public void onEnable() {
+        plugin = this;
+
+        Config.load();
+
         Chunky.getModuleManager().registerModule(this);
 
         chunkyEvents = new ChunkyEvents();
@@ -34,12 +47,31 @@ public class ChunkyVillage extends JavaPlugin {
     private void registerChunkyCommands(){
         ChunkyCommand root = Chunky.getModuleManager().getCommandByName("chunky");
         try {
-            ChunkyCommand town = new ChunkyCommand("town", Arrays.asList("t"),"Commands for ChunkyVillage",Arrays.asList("/chunky town <name> or /c t <name>"),new Town(),root);
+            ChunkyCommand town = new ChunkyCommand("town", Arrays.asList("t"),"Teleport to your town.",Arrays.asList("/chunky town or /c t <name>"),new Town(),root);
             ChunkyCommand newTown = new ChunkyCommand("new", Arrays.asList("n"),"Create a new town",Arrays.asList("/c town new or /c t n"),new NewTown(),town);
             ChunkyCommand forSale = new ChunkyCommand("forsale", Arrays.asList("fs"),"Sets land for sale",Arrays.asList("/c town forsale or /c t fs"),new ForSale(),town);
+            ChunkyCommand addResident = new ChunkyCommand("add", Arrays.asList("a"),"Adds resident to town.",Arrays.asList("/c town add <player> or /c t a <player>"),new AddResident(),town);
+            ChunkyCommand kickResident = new ChunkyCommand("remove", Arrays.asList("r"),"Removes resident from town.",Arrays.asList("/c town remove <player> or /c t r <player>"),new KickResident(),town);
+            ChunkyCommand set = new ChunkyCommand("set", Arrays.asList("s"),"Change various options.",Arrays.asList("/c town set ? or /c t s ?"),new Set(),town);
+            ChunkyCommand setName = new ChunkyCommand("name", Arrays.asList("n"),"Sets town name.",Arrays.asList("/c town set name <name> or /c t s n <name>"),new SetName(),set);
+
+            ChunkyCommand toggle = new ChunkyCommand("toggle", Arrays.asList("t"),"Toggle various options.",Arrays.asList("/c town toggle ? or /c t t ?"),new Toggle(),town);
+            ChunkyCommand toggleAssistant = new ChunkyCommand("assistant", Arrays.asList("a"),"Sets an assistant.",Arrays.asList("/c town toggle assistant <name> or /c t t a <name>"),new Toggle(),set);
+
             Chunky.getModuleManager().registerCommand(town);
             Chunky.getModuleManager().registerCommand(newTown);
+
             Chunky.getModuleManager().registerCommand(forSale);
+
+            Chunky.getModuleManager().registerCommand(addResident);
+            Chunky.getModuleManager().registerCommand(kickResident);
+
+            Chunky.getModuleManager().registerCommand(set);
+            Chunky.getModuleManager().registerCommand(setName);
+
+            Chunky.getModuleManager().registerCommand(toggle);
+            Chunky.getModuleManager().registerCommand(toggleAssistant);
+
         } catch (ChunkyUnregisteredException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
